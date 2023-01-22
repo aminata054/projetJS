79c1c550317bd34b5ae682ff417fc27240a3d2ed
bamxna <tinaami054@gmail.com>
2023-01-22 21:33:51 +0000

    test

--- examen.js
+++ examen.js
@@ -116,7 +116,7 @@ function soummetreTest() {
                     }
                 }
             }
-            break;
+            break; 
         case 'B2': console.log(corrections.b2); break;
         case 'B3': console.log(corrections.b3); break;
         case 'B4': console.log(corrections.b4); break;
@@ -131,3 +131,4 @@ function soummetreTest() {
         default: console.log('default');
     }
 }
+//test 
